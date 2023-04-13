9fc4d2bbdea568833c1057958b15f1213b584b33
Abhijeet Dhupia <abhijeet@bigvisionllc.com>
2023-04-13 12:31:43 +0530

    Testing

---
+++ precache-manifest.f8458dc5ec7a7e3e3c192379a47297ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdea9535d7e136e06244",
+    "url": "/static/css/main.0368a1e1.chunk.css"
+  },
+  {
+    "revision": "fdea9535d7e136e06244",
+    "url": "/static/js/main.fdea9535.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "c3e419cfd1f1f78582ca",
+    "url": "/static/js/2.c3e419cf.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "670509ecde49796381979776530acb36",
+    "url": "/index.html"
+  }
+];
